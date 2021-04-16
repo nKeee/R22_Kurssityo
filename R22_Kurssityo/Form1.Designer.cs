35c5c3da3f7852b3de6e4836193c13d518a09a08
nKeee <77081614+nKeee@users.noreply.github.com>
2021-04-16 12:17:39 +0300

    Tein toisen napin

--- R22_Kurssityo/Form1.Designer.cs
+++ R22_Kurssityo/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace R22_Kurssityo
         {
             this.btn1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn1
@@ -52,11 +53,21 @@ namespace R22_Kurssityo
             this.label1.TabIndex = 1;
             this.label1.Text = "TESTI";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Toinen nappi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(458, 236);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
@@ -70,6 +81,7 @@ namespace R22_Kurssityo
 
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
