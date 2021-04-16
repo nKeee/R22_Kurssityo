b358710d1310ca43711ad3c8e8f9f68e5002cb85
Joungi <korhonenj2@gmail.com>
2021-04-16 12:15:07 +0300

    Labelin tein testiksi

--- R22_Kurssityo/Form1.Designer.cs
+++ R22_Kurssityo/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace R22_Kurssityo
         private void InitializeComponent()
         {
             this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn1
@@ -42,21 +43,33 @@ namespace R22_Kurssityo
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TESTI";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(458, 236);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
