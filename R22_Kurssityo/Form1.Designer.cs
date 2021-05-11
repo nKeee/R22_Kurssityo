25cd977aacb5558e1a79ea91894918c58f214a62
nKeee <77081614+nKeee@users.noreply.github.com>
2021-05-11 17:43:13 +0300

    Irroitettu viimeisin commit kaatumisen takia

--- R22_Kurssityo/Form1.Designer.cs
+++ R22_Kurssityo/Form1.Designer.cs
@@ -662,7 +662,6 @@ namespace R22_Kurssityo
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(771, 536);
             this.tabControl1.TabIndex = 6;
-            this.tabControl1.Click += new System.EventHandler(this.tabControl1_Click);
             // 
             // tab_laskutus
             // 
